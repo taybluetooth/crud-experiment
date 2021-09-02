b2ead31a2f48d29da5dbfd54a894524bf8ad2254
taybluetooth <callumtaylorbusiness@gmail.com>
2021-09-02 12:42:43 +0100

    Added get product by id route.

--- server/controllers/product_controllers.js
+++ server/controllers/product_controllers.js
@@ -1,103 +1,115 @@
 import mysql from "mysql";
 import dbConfig from "../config/db_config.js";
 import fs from "fs";
-import * as path from 'path';
+import * as path from "path";
 
 // Global Directory Declaration
 const __dirname = path.resolve();
 // Long SQL Query Files As Raw Strings
-const updateSql = fs.readFileSync(path.join(__dirname, '/sql', 'update_product.sql').toString()).toString();
+const updateSql = fs
+  .readFileSync(path.join(__dirname, "/sql", "update_product.sql").toString())
+  .toString();
 // MySQL Connection Pool
 const pool = mysql.createPool(dbConfig);
 
 const ProductController = {
-
-    get(req, res) {
-        pool.getConnection((err, connection) => {
-            if (err) throw err;
-            connection.query(`SELECT * FROM products`, (err, rows) => {
-                connection.release();
-                if (!err) {
-                    console.log('SERVER SUCCESSFULLY FETCHED ALL PRODUCTS FROM DATABASE.');
-                    res.status(200).send(rows);
-                } else {
-                    res.status(404);
-                    console.log(err);
-                }
-            });
-        });
-    },
-
-    insert(req, res) {
-        pool.getConnection((err, connection) => {
-            if (err) throw err;
-            const data = req.body;
-            connection.query(
-                `INSERT INTO products SET ?`, data,
-                (err, rows) => {
-                    connection.release();
-                    if (!err) {
-                        console.log('SERVER INSERTED PRODUCT INTO DATABASE.');
-                        res.status(201).send(rows);
-                    } else {
-                        res.status(400);
-                        console.log(err);
-                    };
-                },
+  getOne(req, res) {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      const id = req.params.id;
+      connection.query(
+        `SELECT * FROM products WHERE id=?`,
+        [id],
+        (err, rows) => {
+          connection.release();
+          if (!err && rows[0]) {
+            console.log(
+              "SERVER SUCCESSFULLY FETCHED SPECIFIC PRODUCT FROM DATABASE."
             );
-        }, );
-    },
-
-    update(req, res) {
-        pool.getConnection((err, connection) => {
-            if (err) throw err;
-            const {
-                id,
-                name,
-                price,
-                type,
-                image,
-            } = req.body;
-            connection.query(
-                updateSql,
-                [name, price, type, image, id],
-                (err, rows) => {
-                    connection.release();
-                    if (!err) {
-                        console.log('SERVER SUCCESSFULLY UPDATED PRODUCT IN DATABASE.');
-                        res.status(200).send(rows);
-                    } else {
-                        res.status(400);
-                        console.log(err);
-                    };
-                },
+            res.status(200).send(rows[0]);
+          } else {
+            res.status(404);
+            console.log(
+              "SERVER COULD NOT FIND A PRODUCT FROM THE DATABASE MATCHING THAT DESCRIPTION."
             );
-        }, );
-    },
+          }
+        }
+      );
+    });
+  },
 
-    delete(req, res) {
-        pool.getConnection((err, connection) => {
-            if (err) throw err;
-            const {
-                id
-            } = req.body;
-            connection.query(
-                `DELETE FROM products WHERE id=?`,
-                [id],
-                (err, rows) => {
-                    connection.release();
-                    if (!err) {
-                        console.log('SERVER SUCCESSFULLY DELETED BILL FROM DATABASE.');
-                        res.status(200).send(rows);
-                    } else {
-                        res.status(406);
-                        console.log(err);
-                    };
-                },
-            );
-        }, );
-    },
+  getAll(req, res) {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query(`SELECT * FROM products`, (err, rows) => {
+        connection.release();
+        if (!err) {
+          console.log(
+            "SERVER SUCCESSFULLY FETCHED ALL PRODUCTS FROM DATABASE."
+          );
+          res.status(200).send(rows);
+        } else {
+          res.status(404);
+          console.log(err);
+        }
+      });
+    });
+  },
 
-}
+  insert(req, res) {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      const data = req.body;
+      connection.query(`INSERT INTO products SET ?`, data, (err, rows) => {
+        connection.release();
+        if (!err) {
+          console.log("SERVER INSERTED PRODUCT INTO DATABASE.");
+          res.status(201).send(rows);
+        } else {
+          res.status(400);
+          console.log(err);
+        }
+      });
+    });
+  },
+
+  update(req, res) {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      const { id, name, price, type, image } = req.body;
+      connection.query(
+        updateSql,
+        [name, price, type, image, id],
+        (err, rows) => {
+          connection.release();
+          if (!err) {
+            console.log("SERVER SUCCESSFULLY UPDATED PRODUCT IN DATABASE.");
+            res.status(200).send(rows);
+          } else {
+            res.status(400);
+            console.log(err);
+          }
+        }
+      );
+    });
+  },
+
+  delete(req, res) {
+    pool.getConnection((err, connection) => {
+      if (err) throw err;
+      const { id } = req.body;
+      connection.query(`DELETE FROM products WHERE id=?`, [id], (err, rows) => {
+        connection.release();
+        if (!err) {
+          console.log("SERVER SUCCESSFULLY DELETED BILL FROM DATABASE.");
+          res.status(200).send(rows);
+        } else {
+          res.status(406);
+          console.log(err);
+        }
+      });
+    });
+  },
+};
 
 export default ProductController;
--- server/routes/app_v1_routes.js
+++ server/routes/app_v1_routes.js
@@ -4,7 +4,8 @@ import ProductController from "../controllers/product_controllers.js";
 const router = Express.Router();
 
 // User Routes
-router.get('/products', ProductController.get);
+router.get('/products/:id', ProductController.getOne);
+router.get('/products', ProductController.getAll);
 router.post('/product/add', ProductController.insert);
 router.delete('/product/delete', ProductController.delete);
 router.put('/product/update', ProductController.update);
